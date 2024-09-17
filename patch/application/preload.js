f93c66aabeab1097023249ff8e5f049c177315a4
XYenon <i@xyenon.bid>
2024-09-17 21:03:08 +0800

    Add LiteLoaderQQNT

---
+++ patch/application/preload.js
@@ -0,0 +1,38 @@
+// 加载渲染进程
+document.addEventListener("DOMContentLoaded", () => {
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = `local://root/src/renderer.js`;
+    document.head.prepend(script);
+});
+
+
+// 运行外部脚本
+Object.defineProperty(globalThis, "runPreloadScript", {
+    configurable: false,
+    value: content => new Function(
+        "require",
+        "process",
+        "Buffer",
+        "global",
+        "setImmediate",
+        "clearImmediate",
+        "exports",
+        "module",
+        content
+    )(...arguments)
+});
+
+
+// 加载插件 Preload
+(async () => {
+    runPreloadScript(await (await fetch(`local://root/src/liteloader_api/preload.js`)).text());
+    runPreloadScript(await (await fetch(`local://root/src/loader_core/preload.js`)).text());
+})();
+
+
+// 加载 QQNT Preload（这后面会被自动处理）
+{try{ const { contextBridge } = require('electron');
+contextBridge.exposeInMainWorld('electron',{load: (file) => { require('../major.node').load(file, module);}});
+}catch{}
+require('../major.node').load('p_preload', module);}
